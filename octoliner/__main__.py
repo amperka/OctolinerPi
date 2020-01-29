48523a9f5e35d677889a474d9cdf1d29b27d1f48
AlexErAmp <alexer@amperka.ru>
2020-01-29 14:28:41 +0300

    Add __main__.py

---
+++ octoliner/__main__.py
@@ -0,0 +1,24 @@
+import time
+
+from .octoliner import Octoliner
+
+
+# Create an object for working with 8-channel line sensor.
+octo = Octoliner(42)
+# Set the sensitivity of the photodetectors.
+octo.set_sensitivity(1.0)
+
+
+def main():
+    try:
+        while True:
+            # Print the current line position in the console.
+            print(octo.track_line())
+            # Wait 0.5 seconds.
+            time.sleep(0.5)
+    except KeyboardInterrupt:
+        pass
+
+
+if __name__ == "__main__":
+    main()
